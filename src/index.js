c44debac612b2e8880c16f50887629c9e668044a
Palesa-Tsotetsi <152623456+Palesa-Tsotetsi@users.noreply.github.com>
2024-03-07 09:46:39 +0200

    added github account

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Weather />
+    
+
+    Project coded by Palesa Tsotetsi and can be found on <a href="https://github.com/Palesa-Tsotetsi?tab=repositories">Github</a>
   </React.StrictMode>
 );
 
